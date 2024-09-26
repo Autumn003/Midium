7d3fe26e3e300bc85a91af27927c80407a3d799c
Hemant <davidbroddy1@gmail.com>
2024-09-26 16:54:06 +0530

    "Added Blogs route and component, updated Auth component to display error alert"

--- client/src/components/Auth.tsx
+++ client/src/components/Auth.tsx
@@ -21,7 +21,7 @@ export const Auth = ({ type }: { type: "signup" | "signin" }) =>{
             localStorage.setItem("token", jwt);
             navigate("/blogs");
         } catch (error) {
-            
+            alert("Error while signing up");
         }
     }
 
