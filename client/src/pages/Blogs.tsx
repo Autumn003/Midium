c1dcd8405c5410899079f3ee0fc84bfc1683fbdb
Hemant <davidbroddy1@gmail.com>
2024-09-27 21:44:23 +0530

    "Added BlogSkeleton component to display while blogs are loading"

--- client/src/pages/Blogs.tsx
+++ client/src/pages/Blogs.tsx
@@ -1,16 +1,26 @@
 import { AppBar } from "../components/AppBar"
 import { BlogCard } from "../components/BlogCard"
+import { BlogSkeleton } from "../components/BlogSkeleton";
 import { useBlogs } from "../hooks"
 
 
 export const Blogs = () => {
     const {loading, blogs} = useBlogs();
 
-    if(loading){
+    if (loading) {
       return <div>
-        Loading...
+          <AppBar /> 
+          <div  className="flex justify-center">
+              <div>
+                  <BlogSkeleton />
+                  <BlogSkeleton />
+                  <BlogSkeleton />
+                  <BlogSkeleton />
+                  <BlogSkeleton />
+              </div>
+          </div>
       </div>
-    }
+  }
 
     return (
     <>
